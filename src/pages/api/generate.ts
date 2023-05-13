1350867e7557b79567637120d1286a4642ad64b2
liuchengda <33249513+wycsnxj@users.noreply.github.com>
2023-05-13 19:49:58 +0800

    Update generate.ts

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -35,59 +35,37 @@ export const post: APIRoute = async (context) => {
   return new Response(parseOpenAIStream(response))
 }
 
-// 定义一个函数，用来解析开放人工智能联盟的返回结果
+// 定义一个异步函数,用来解析openAI的返回结果
 const parseOpenAIStream = async (response: Response) => {
-  // 定义一个变量reader，用来获取响应的内容
-  const reader = response.body.getReader();
-  // 定义一些变量，用来存储字符，状态，和上一次的字符
-  let char = "";
-  let done = false;
-  let lastChar = "";
-  let lastLastChar = "";
-  let lastLastLastChar = "";
+  const reader = response.getReader()
+  
+  // 定义变量,存储状态
+  let char = '' 
+  let done = false
+  let lastChar = ''
+  let lastLastChar = ''
+  let lastLastLastChar = ''
 
-  // 循环读取响应的内容，直到结束
+  // 循环读取响应,直到结束
   while (!done) {
-    const { value, done: readerDone } = await reader.read();
-    // 如果有值，就把它解码成字符
+    const { value, done: readerDone } = await reader.read()
+    
+    // 如果有值,解码并替换
     if (value) {
-      char += decoder.decode(value);
-      // 如果字符是换行符，并且上一个字符也是换行符，就跳过
-      if (char === "\n" && lastChar === "\n") {
-        continue;
-      }
-      // 如果字符是换行符，并且上一个字符是三个或四个点号，就跳过
-      if (
-        char === "\n" &&
-        lastChar === "." &&
-        lastLastChar === "." &&
-        lastLastLastChar === "."
-      ) {
-        continue;
-      }
-      if (
-        char === "\n" &&
-        lastChar === "." &&
-        lastLastChar === "." &&
-        lastLastLastChar === "." &&
-        lastLastLastLastChar === "."
-      ) {
-        continue;
-      }
-      // 把chatGPT替换成叽喳GPT
-      char = char.replaceAll("chatGPT", "叽喳GPT");
-      // 把openAI替换成开放人工智能联盟
-      char = char.replaceAll("openAI", "开放人工智能联盟"); 
-      console.log("-----"+char);
-      // 返回字符
-      return char;
+      char = new TextDecoder().decode(value)
+      char = char.replace(/chatGPT/gi, '叽喳GPT')    
+      char = char.replace(/openAI/gi, '开放人工智能联盟')    
     }
-    // 更新状态和上一次的字符
-    done = readerDone;
-    lastLastLastLastChar = lastLastLastChar;
-    lastLastLastChar = lastLastChar;
-    lastLastChar = lastChar;
-    lastChar = char;
-    char = "";
+
+    // 不返回char,直接在函数内处理
+    // char 重置为空字符串
+    char = ''  
+    
+    // 更新状态
+    lastLastLastLastChar = lastLastLastChar  
+    lastLastLastChar = lastLastChar  
+    lastLastChar = lastChar
+    lastChar = char 
+    done = readerDone
   }
 }
