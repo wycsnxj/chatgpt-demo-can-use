0ff8234d2b16a37f6448755edb5eccb500d0d494
wycsnxj <wycsnxj@163.com>
2023-06-02 21:11:39 +0800

    11

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,258 +1,293 @@
-import type { ChatMessage } from '@/types'
-import { createSignal, Index, Show, onMount, onCleanup } from 'solid-js'
+import { Index, Show, createEffect, createSignal, onCleanup, onMount } from 'solid-js'
+import { useThrottleFn } from 'solidjs-use'
+import { generateSignature } from '@/utils/auth'
 import IconClear from './icons/Clear'
 import MessageItem from './MessageItem'
 import SystemRoleSettings from './SystemRoleSettings'
-import { generateSignature } from '@/utils/auth'
-import { useThrottleFn } from 'solidjs-use'
+import ErrorMessageItem from './ErrorMessageItem'
+import type { ChatMessage, ErrorMessage } from '@/types'
+
+// 定义一些常量
+const SCROLL_DELAY = 300 // 滚动延迟
+const PHOTOSPHERE_THICKNESS = 500 // 光球厚度
+const CHROMOSPHERE_THICKNESS = 2500 // 色球厚度
 
 export default () => {
- let inputRef: HTMLTextAreaElement
- const [currentSystemRoleSettings, setCurrentSystemRoleSettings] = createSignal('')
- const [systemRoleEditing, setSystemRoleEditing] = createSignal(false)
- const [messageList, setMessageList] = createSignal<ChatMessage[]>([])
- const [currentAssistantMessage, setCurrentAssistantMessage] = createSignal('')
- const [loading, setLoading] = createSignal(false)
- const [controller, setController] = createSignal<AbortController>(null)
+ let inputRef: HTMLTextAreaElement
+ const [currentSystemRoleSettings, setCurrentSystemRoleSettings] = createSignal('')
+ const [systemRoleEditing, setSystemRoleEditing] = createSignal(false)
+ const [messageList, setMessageList] = createSignal<ChatMessage[]>([])
+ const [currentError, setCurrentError] = createSignal<ErrorMessage>()
+ const [currentAssistantMessage, setCurrentAssistantMessage] = createSignal('')
+ const [loading, setLoading] = createSignal(false)
+ const [controller, setController] = createSignal<AbortController>(null)
+ const [isStick, setStick] = createSignal(false)
 
+ createEffect(() => (isStick() && smoothToBottom()))
 
- onMount(() => {
- try {
- if (localStorage.getItem('messageList')) {
- setMessageList(JSON.parse(localStorage.getItem('messageList')))
- }
- if (localStorage.getItem('systemRoleSettings')) {
- setCurrentSystemRoleSettings(localStorage.getItem('systemRoleSettings'))
- }
- } catch (err) {
- console.error(err)
- }
- 
- window.addEventListener('beforeunload', handleBeforeUnload)
- onCleanup(() => {
- window.removeEventListener('beforeunload', handleBeforeUnload)
- })
- })
+ onMount(() => {
+ let lastPostion = window.scrollY
 
- const handleBeforeUnload = () => {
- localStorage.setItem('messageList', JSON.stringify(messageList()))
- localStorage.setItem('systemRoleSettings', currentSystemRoleSettings())
- }
+ window.addEventListener('scroll', () => {
+ const nowPostion = window.scrollY
+ nowPostion < lastPostion && setStick(false)
+ lastPostion = nowPostion
+ })
 
- const handleButtonClick = async () => {
- const inputValue = inputRef.value
- if (!inputValue) {
- return
- }
- // @ts-ignore
- if (window?.umami) umami.trackEvent('chat_generate')
- inputRef.value = ''
- setMessageList([
- ...messageList(),
- {
- role: 'user',
- content: inputValue,
- },
- ])
- requestWithLatestMessage()
- }
+ try {
+ if (localStorage.getItem('messageList'))
+ setMessageList(JSON.parse(localStorage.getItem('messageList')))
 
- const smoothToBottom = useThrottleFn(() => {
- window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
- }, 300, false, true)
+ if (localStorage.getItem('systemRoleSettings'))
+ setCurrentSystemRoleSettings(localStorage.getItem('systemRoleSettings'))
 
- const requestWithLatestMessage = async () => {
-  setLoading(true)
-  setCurrentAssistantMessage('')
-  const storagePassword = localStorage.getItem('pass')
-  try {
-    const controller = new AbortController()
-    setController(controller)
-    const requestMessageList = [...messageList()]
-    if (currentSystemRoleSettings()) {
-      requestMessageList.unshift({
-        role: 'system',
-        content: currentSystemRoleSettings(),
-      })
-    }
-    const timestamp = Date.now()
-    const response = await fetch('/api/generate', {
-      method: 'POST',
-      body: JSON.stringify({
-        messages: requestMessageList,
-        time: timestamp,
-        pass: storagePassword,
-        sign: await generateSignature({
-          t: timestamp,
-          m: requestMessageList?.[requestMessageList.length - 1]?.content || '',
-        }),
-      }),
-      signal: controller.signal,
-    })
-    if (!response.ok) {
-      throw new Error(response.statusText)
-    }
-    const data = response.body
-    if (!data) {
-      throw new Error('No data')
-    }
-    const reader = data.getReader()
-    const decoder = new TextDecoder('utf-8')
-    let done = false
-    while (!done) {
-      const { value, done: readerDone } = await reader.read()
-      if (value) {
-        let char = decoder.decode(value)
-        if (char === '\n' && currentAssistantMessage().endsWith('\n')) {
-          continue
-        }
-        const replaceSensitiveWords = (text: string) => {
-          // 修改了函数名
-          const replaceSensitiveChar = (char: string, buffer: string) => {
-            const sensitiveWordsMap = [
-              { regex: /chatGPT/gi, replacement: "叽喳聊天" },
-              { regex: /chat GPT/gi, replacement: "叽喳聊天" },
-              { regex: /openAI/gi, replacement: "开放人工智能" },
-            ];
-            
-            let newText = buffer + char;
-            
-            sensitiveWordsMap.forEach(({ regex, replacement }) => {
-              newText = newText.replace(regex, replacement);
-            });
-            
-            return newText.slice(buffer.length);
-          };
-          
-          if (char) {
-            // 对返回的字符进行敏感词汇替换
-            console.log('------'+char);
-            // 对返回的字符进行敏感词汇替换
-            // 调用了修改后的函数名
-            const replacedChar = replaceSensitiveChar(char, currentAssistantMessage());
-            setCurrentAssistantMessage(currentAssistantMessage() + replacedChar);
-          }
-          smoothToBottom()
-        }
-        done = readerDone
-      }}
- } catch (e) { // 这里缺少了一个左花括号，应该是 catch (e) { 
-      console.error(e)
-      setLoading(false)
-      setController(null)
-      return
-    }
-    archiveCurrentMessage()
-  }
+ if (localStorage.getItem('stickToBottom') === 'stick')
+ setStick(true)
+ } catch (err) {
+ console.error(err)
+ }
 
-  const archiveCurrentMessage = () => {
-    if (currentAssistantMessage()) {
-      setMessageList([
-        ...messageList(),
-        {
-          role: 'assistant',
-          content: currentAssistantMessage(),
-        },
-      ])
-      setCurrentAssistantMessage('')
-      setLoading(false)
-      setController(null)
-      inputRef.focus()
-    }
-  }
+ window.addEventListener('beforeunload', handleBeforeUnload)
+ onCleanup(() => {
+ window.removeEventListener('beforeunload', handleBeforeUnload)
+ })
+ })
 
-  const clear = () => {
-    inputRef.value = ''
-    inputRef.style.height = 'auto';
-    setMessageList([])
-    setCurrentAssistantMessage('')
-    setCurrentSystemRoleSettings('')
-  }
+ const handleBeforeUnload = () => {
+ localStorage.setItem('messageList', JSON.stringify(messageList()))
+ localStorage.setItem('systemRoleSettings', currentSystemRoleSettings())
+ isStick() ? localStorage.setItem('stickToBottom', 'stick') : localStorage.removeItem('stickToBottom')
+ }
 
-  const stopStreamFetch = () => {
-    if (controller()) {
-      controller().abort()
-      archiveCurrentMessage()
-    }
-  }
+ const handleButtonClick = async() => {
+ const inputValue = inputRef.value
+ if (!inputValue)
+ return
 
-  const retryLastFetch = () => {
-    if (messageList().length > 0) {
-      const lastMessage = messageList()[messageList().length - 1]
-      console.log(lastMessage)
-      if (lastMessage.role === 'assistant') {
-        setMessageList(messageList().slice(0, -1))
-        requestWithLatestMessage()
-      }
-    }
-  }
+ inputRef.value = ''
+ setMessageList([
+ ...messageList(),
+ {
+ role: 'user',
+ content: inputValue,
+ },
+ ])
+ requestWithLatestMessage()
+ instantToBottom()
+ }
 
-  const handleKeydown = (e: KeyboardEvent) => {
-    if (e.isComposing || e.shiftKey) {
-      return
-    }
-    if (e.key === 'Enter') {
-      handleButtonClick()
-    }
-  }
+ // 封装一个滚动到底部的函数
+ const scrollToBottom = (behavior: ScrollBehavior) => {
+   window.scrollTo({ top: document.body.scrollHeight, behavior })
+ }
 
-  return (
-    <div my-6>
-      <SystemRoleSettings
-        canEdit={() => messageList().length === 0}
-        systemRoleEditing={systemRoleEditing}
-        setSystemRoleEditing={setSystemRoleEditing}
-        currentSystemRoleSettings={currentSystemRoleSettings}
-        setCurrentSystemRoleSettings={setCurrentSystemRoleSettings}
-      />
-      <Index each={messageList()}>
-        {(message, index) => (
-          <MessageItem
-            role={message().role}
-            message={message().content}
-            showRetry={() => (message().role === 'assistant' && index === messageList().length - 1)}
-            onRetry={retryLastFetch}
-          />
-        )}
-      </Index>
-      {currentAssistantMessage() && (
-        <MessageItem
-          role="assistant"
-          message={currentAssistantMessage}
-        />
-      )}
-      <Show
-        when={!loading()}
-        fallback={() => (
-          <div class="gen-cb-wrapper">
-            <span>AI is thinking...</span>
-            <div class="gen-cb-stop" onClick={stopStreamFetch}>Stop</div>
-          </div>
-        )}
-      >
-        <div class="gen-text-wrapper" class:op-50={systemRoleEditing()}>
-          <textarea
-            ref={inputRef!}
-            disabled={systemRoleEditing()}
-            onKeyDown={handleKeydown}
-            placeholder="Enter something..."
-            autocomplete="off"
-            autofocus
-            onInput={() => {
-              inputRef.style.height = 'auto';
-              inputRef.style.height = inputRef.scrollHeight + 'px';
-            }}
-            rows="1"
-            class='gen-textarea'
-          />
-          <button onClick={handleButtonClick} disabled={systemRoleEditing()} gen-slate-btn>
-            发送
-          </button>
-          <button title="Clear" onClick={clear} disabled={systemRoleEditing()} gen-slate-btn>
-            <IconClear />
-          </button>
-        </div>
-      </Show>
-    </div>
-  )
-}
+ const smoothToBottom = useThrottleFn(() => {
+ scrollToBottom('smooth')
+ }, SCROLL_DELAY, false, true)
+
+ const instantToBottom = () => {
+ scrollToBottom('instant')
+ }
+
+ const requestWithLatestMessage = async() => {
+ setLoading(true)
+ setCurrentAssistantMessage('')
+ setCurrentError(null)
+ const storagePassword = localStorage.getItem('pass')
+ try {
+ const controller = new AbortController()
+ setController(controller)
+ const ruleSetting = import.meta.env.RULE_SETTING
+ const requestMessageList = [
+  {
+    role: 'system',
+    content: ruleSetting
+  },
+  ...messageList()
+  ]
+ const timestamp = Date.now()
+ const response = await fetch('/api/generate', {
+ method: 'POST',
+ body: JSON.stringify({
+ messages: requestMessageList,
+ time: timestamp,
+ pass: storagePassword,
+ sign: await generateSignature({
+ t: timestamp,
+ m: requestMessageList?.[requestMessageList.length - 1]?.content || '',
+ }),
+ }),
+ signal: controller.signal,
+ })
+ if (!response.ok) {
+ const error = await response.json()
+ console.error(error.error)
+ setCurrentError({
+ code: "666", // 
+ message: "试用版次数已达到上限（每分钟3 次）。请在 20 秒后再试或者继续Generate"
+ })
+ throw new Error('Request failed')
+ }
+ const data = response.body
+ if (!data)
+ throw new Error('No data')
+
+ const reader = data.getReader()
+ const decoder = new TextDecoder('utf-8')
+ // 把文字替换逻辑添加到读取数据的过程中
+ let done = false
+
+ while (!done) {
+ const { value, done: readerDone } = await reader.read()
+ if (value) {
+ let char = decoder.decode(value)
+ if (char === '\n' && currentAssistantMessage().endsWith('\n'))
+ continue
+
+ // 定义一个敏感词映射表
+ const sensitiveWordsMap = [
+   { regex: /chatGPT/gi, replacement: "叽喳聊天" },
+   { regex: /chat GPT/gi, replacement: "叽喳聊天" },
+   { regex: /openAI/gi, replacement: "叽喳聊天" },
+   { regex: /open AI/gi, replacement: "叽喳聊天" },
+ ];
+ // 替换敏感词
+ let newText = currentAssistantMessage() + char;
+ sensitiveWordsMap.forEach(({ regex, replacement }) => {
+   newText = newText.replace(regex, replacement);
+ });
+ setCurrentAssistantMessage(newText);
+
+ isStick() && instantToBottom()
+ }
+ done = readerDone
+ }
+ } catch (e) {
+ console.error(e)
+ setLoading(false)
+ setController(null)
+ return
+ }
+ archiveCurrentMessage()
+ isStick() && instantToBottom()
+ }
+
+ const archiveCurrentMessage = () => {
+ if (currentAssistantMessage()) {
+ setMessageList([
+ ...messageList(),
+ {
+ role: 'assistant',
+ content: currentAssistantMessage(),
+ },
+ ])
+ setCurrentAssistantMessage('')
+ setLoading(false)
+ setController(null)
+ inputRef.focus()
+ }
+ }
+
+ const clear = () => {
+ inputRef.value = ''
+ inputRef.style.height = 'auto'
+ setMessageList([])
+ setCurrentAssistantMessage('')
+ setCurrentError(null)
+ }
+
+ const stopStreamFetch = () => {
+ if (controller()) {
+ controller().abort()
+ archiveCurrentMessage()
+ }
+ }
+
+ const retryLastFetch = () => {
+ if (messageList().length > 0) {
+ const lastMessage = messageList()[messageList().length - 1]
+ if (lastMessage.role === 'assistant')
+ setMessageList(messageList().slice(0, -1))
+
+ requestWithLatestMessage()
+ }
+ }
+
+ const handleKeydown = (e: KeyboardEvent) => {
+ if (e.isComposing || e.shiftKey)
+ return
+
+ if (e.keyCode === 13) {
+ e.preventDefault()
+ handleButtonClick()
+ }
+ }
+
+ return (
+ <div my-6>
+ <SystemRoleSettings
+ canEdit={() => messageList().length === 0}
+ systemRoleEditing={systemRoleEditing}
+ setSystemRoleEditing={setSystemRoleEditing}
+ currentSystemRoleSettings={currentSystemRoleSettings}
+ setCurrentSystemRoleSettings={setCurrentSystemRoleSettings}
+ />
+ <Index each={messageList()}>
+ {(message, index) => (
+ <MessageItem
+ role={message().role}
+ message={message().content}
+ showRetry={() => (message().role === 'assistant' && index === messageList().length - 1)}
+ onRetry={retryLastFetch}
+ />
+ )}
+ </Index>
+ {currentAssistantMessage() && (
+ <MessageItem
+ role="assistant"
+ message={currentAssistantMessage}
+ />
+ )}
+ { currentError() && <ErrorMessageItem data={currentError()} onRetry={retryLastFetch} /> }
+ <Show
+ when={!loading()}
+ fallback={() => (
+ <div class="gen-cb-wrapper">
+ <span>AI is thinking...</span>
+ <div class="gen-cb-stop" onClick={stopStreamFetch}>Stop</div>
+ </div>
+ )}
+ >
+ <div class="gen-text-wrapper" class:op-50={systemRoleEditing()}>
+ <textarea
+ ref={inputRef!}
+ disabled={systemRoleEditing()}
+ onKeyDown={handleKeydown}
+ placeholder="Enter something..."
+ autocomplete="off"
+ autofocus
+ onInput={() => {
+ inputRef.style.height = 'auto'
+ inputRef.style.height = `${inputRef.scrollHeight}px`
+ }}
+ rows="1"
+ class="gen-textarea"
+ />
+ <button onClick={handleButtonClick} disabled={systemRoleEditing()} gen-slate-btn>
+ Send
+ </button>
+ <button title="Clear" onClick={clear} disabled={systemRoleEditing()} gen-slate-btn>
+ <IconClear />
+ </button>
+ </div>
+ </Show>
+ <div class="fixed bottom-5 left-5 rounded-md hover:bg-slate/10 w-fit h-fit transition-colors active:scale-90" class:stick-btn-on={isStick()}>
+	        <div>
+	          <button class="p-2.5 text-base" title="stick to bottom" type="button" onClick={() => setStick(!isStick())}>
+	            <div i-ph-arrow-line-down-bold />
+	          </button>
+	        </div>
+	      </div>
+	    </div>
+	  )
+	}
